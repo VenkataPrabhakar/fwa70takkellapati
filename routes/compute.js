f73fa27299fea2766a91153244e4e130b38e59f2
VenkataPrabhakar <89261443+VenkataPrabhakar@users.noreply.github.com>
2021-10-26 23:39:40 -0500

    final

--- routes/compute.js
+++ routes/compute.js
@@ -1,13 +1,14 @@
-var express=require('express')
- 
-var router=express.Router();
- 
-router.get('/',(req,res)=>{
-    let ran=Math.floor(Math.random()*100)+1;
-    let data=req.query.x;
-    console.log(data,ran);
-    res.write('${Math.hypot(ran)}'
+var express = require('express')
+
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    let ran = Math.floor(Math.random() * 100) + 1;
+    let data = req.query.x;
+    console.log(data, ran);
+    res.send('math.log2(' + ran + ') is :' + Math.log2(ran) + '  math.cosh(' + ran + ') is :' + Math.cosh(ran) +
+     '     math.floor(' + ran + ') is :' + Math.floor(ran)
     );
 })
- 
-module.exports=router
+
+module.exports = router
